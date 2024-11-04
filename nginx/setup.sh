88cd0062729c4380bccb0fde89be9a8ba27cfc6a
Chavalit Koweerawong <chavalit.kow@gmail.com>
2024-11-04 15:34:09 +0700

    setup

---
+++ nginx/setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# bash setup.sh
+
+# source code
+# sudo mkdir -p /var/www/plants.samkhok.org
+# cd /var/www/plants.samkhok.org
+# git clone https://github.com/arc6828/plant-class
+
+# for Laravel
+cd /var/www/plants.samkhok.org/plant-class
+sudo chmod 777 -R storage
+sudo chown -R www-data.www-data storage
+composer install
+cp .env.example .env
+php artisan key:generate
+
+# config Laravel .env manually
+
+# nginx
+cp nginx/plants.samkhok.org.conf /etc/nginx/sites-available/plants.samkhok.org
+sudo ln -s /etc/nginx/sites-available/plants.samkhok.org /etc/nginx/sites-enabled/plants.samkhok.org
+
+sudo service nginx restart
+
+# certbot 
