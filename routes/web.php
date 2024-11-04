a695e7734544c804e611c88d93bc2861e09b28bb
Chavalit Koweerawong <chavalit.kow@gmail.com>
2024-11-04 21:48:35 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,10 @@ use App\Http\Controllers\PlantClassificationController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    // return view('welcome');
+    return redirect()->route("upload.form");
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
