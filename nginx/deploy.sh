2c5101413ad393f1570e6a63542bd7ab0aec3ef8
Chavalit Koweerawong <chavalit.kow@gmail.com>
2025-02-08 12:05:53 +0700

    Create deploy.sh

---
+++ nginx/deploy.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# cp nginx deploy.sh
+# chmod +x script.sh
+PROJECT_PATH="/var/www/plants.samkhok.org/plant-class"
+cd $PROJECT_PATH
+
+# install deploy.sh outside git
+cp nginx/deploy.sh $PROJECT_PATH/../deploy.sh
+chmod +x $PROJECT_PATH/../deploy.sh
+
+# Pull the latest changes
+git pull
+
+# Install dependencies
+composer install
+npm install
+npm run build
+
+# Run migrations (if needed)
+# php artisan migrate
+
+# Clear and cache configurations
+# php artisan cache:clear
+# php artisan config:clear
+# php artisan config:cache
+php artisan route:cache
+
+# php artisan storage:link
+
+# Set correct permissions
+# chown -R www-data:www-data $PROJECT_PATH
+# chmod -R 775 $PROJECT_PATH/storage
+# chmod -R 775 $PROJECT_PATH/storage $PROJECT_PATH/bootstrap/cache
+
+
+# Restart PHP & queue workers (if needed)
+# php artisan queue:restart
+# systemctl restart php8.2-fpm
+
+echo "Deployment completed at $(date)" >> /var/log/deploy.log
+
+
