8dfbf033b7a518d625c9fb15bcfa009bbec06efb
Chavalit Koweerawong <chavalit.kow@gmail.com>
2025-09-23 03:19:34 +0700

    Update PlantIdentificationController.php

--- app/Http/Controllers/PlantIdentificationController.php
+++ app/Http/Controllers/PlantIdentificationController.php
@@ -4,15 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Services\GeminiService;
 use App\Services\ImageService;
+use Exception;
 use Illuminate\Http\Request;
 
 class PlantIdentificationController extends Controller
 {
-    
+
     public function showUploadForm()
     {
         return view('plant-identification');
-    }    
+    }
 
     // return string
     public function identifyPlant(Request $request, GeminiService $geminiService, ImageService $imageService)
@@ -22,12 +23,16 @@ class PlantIdentificationController extends Controller
         ]);
         $path = $request->file('plantImage')->store('temp', 'public');
         // Resize and save the image using ImageService
-        $path = $imageService->processAndSave($path);
+        try {
+            $path = $imageService->processAndSave($path);
+        } catch (Exception $e) {
+            // return back()->with('error', 'เกิดข้อผิดพลาดในการประมวลผลภาพ: ' . $e->getMessage());
+        }
         // เก็บไฟล์ภาพชั่วคราว
         // $path = $file->store('temp', 'public');
         // $imagePath = asset('storage/' . $path);
 
-        $result = $geminiService->classifyImage($path);       
+        $result = $geminiService->classifyImage($path);
 
         return back()->with('result', $result)->with('imagePath', $path);
     }
